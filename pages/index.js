6083fae9f93e10d4922e53b72123610bcca885d4
Brian Mwiruki <bmwiruki@safaricom.co.ke>
2023-04-06 11:11:13 +0300

    Image styling

--- pages/index.js
+++ pages/index.js
@@ -71,7 +71,9 @@ export default function Home() {
         </div>
         <div className='lg:flex gap-10'>
           <div className="text-center justify-center shadow-lg p-10 rounded-xl my-10">
-            <Image src={design} width={100} height={100} />
+            <div className='flex justify-center'>
+              <Image src={design} width={100} height={100}/>
+            </div>
             <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
             <p className='py-2'>
               Creating elegant designs suited for your needs following core design theory.
@@ -83,7 +85,9 @@ export default function Home() {
           </div>
 
           <div className='text-center shadow-lg p-10 rounded-xl my-10'>
-            <Image src={code} width={100} height={100} />
+            <div className='flex justify-center'>
+             <Image src={code} width={100} height={100} />
+            </div>
             <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
             <p className='py-2'>
               Creating elegant designs suited for your needs following core design theory.
@@ -95,7 +99,9 @@ export default function Home() {
           </div>
 
           <div className='text-center shadow-lg p-10 rounded-xl my-10'>
-            <Image src={consulting} width={100} height={100} />
+            <div className='flex justify-center'>
+              <Image src={consulting} width={100} height={100} />
+            </div>
             <h3 className='text-lg font-medium pt-8 pb-2'>Beautiful Designs</h3>
             <p className='py-2'>
               Creating elegant designs suited for your needs following core design theory.
