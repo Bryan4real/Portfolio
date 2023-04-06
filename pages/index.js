5ffc72318cba713876e7ed5caf8f78299704a000
Brian Mwiruki <bmwiruki@safaricom.co.ke>
2023-04-06 12:37:25 +0300

    socials link implementation

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from 'next/head';
 import {BsFillMoonStarsFill} from 'react-icons/bs';
-import {AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube} from 'react-icons/ai';
+import {AiFillGithub, AiFillLinkedin, AiFillMail} from 'react-icons/ai';
 import Image from 'next/image';
 import deved from '../public/dev-ed-wave.png';
 import design from '../public/design.png';
@@ -50,9 +50,9 @@ export default function Home() {
             </p>
           </div>
           <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
-            <AiFillTwitterCircle />
-            <AiFillLinkedin />
-            <AiFillYoutube />
+           <a href="https://github.com/Bryan4real"><AiFillGithub /></a> 
+           <a href="https://www.linkedin.com/in/brian-mwiruki-996116161/"><AiFillLinkedin /></a> 
+           <a href="mailto:mwiruki@outlook.com"><AiFillMail /></a> 
           </div>
           <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 overflow-hidden md:h-96 md:w-96">
             <Image src={deved} layout='fill' objectFit='cover'/>
